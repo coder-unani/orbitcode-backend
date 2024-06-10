67253a20529bebd1854b92265b0803714f6716c6
UNANI <coder.unani@gmail.com>
2024-06-11 03:07:24 +0900

    [feat] 비디오 컨텐츠 Edit 페이지 및 기능 제작

--- app/content/views.py
+++ app/content/views.py
@@ -1,9 +1,18 @@
+import os
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
-from django.http import HttpResponseRedirect
-from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView, ListView, DetailView
+
 from app.database.models import Video, Genre, Actor, Staff
+from app.utils.s3client import S3Client
+from app.utils.utils import make_filename, save_file_from_url, get_file_extension, get_file_size
+from config.properties import (
+    AWS_S3_NETFLIX_THUMBNAIL,
+    LOCAL_NETFLIX_THUMBNAIL
+)
 
 DJANGO_LOGIN_URL = "/login/"
 DJANGO_REDIRECT_FIELD_NAME = "next"
@@ -41,7 +50,7 @@ class VideoEdit(LoginRequiredMixin, DetailView):
 
     def post(self, request, *args, **kwargs):
         edit_type = request.POST.get('edit_type')
-        if edit_type == 'basic':
+        if edit_type == "basic":
             video_id = kwargs.get('pk')
             video = Video.objects.get(id=video_id)
             if not video:
@@ -67,12 +76,176 @@ class VideoEdit(LoginRequiredMixin, DetailView):
                 video.is_confirm = is_confirm == "true"
             video.save()
             return HttpResponseRedirect(reverse('content:video-detail', kwargs={'pk': video_id}))
-        elif edit_type == 'genre':
-            pass
-        elif edit_type == 'actor':
-            pass
-        elif edit_type == 'staff':
-            pass
+        elif edit_type == "thumbnail":
+            video_id = kwargs.get('pk')
+            video = Video.objects.get(id=video_id)
+            thumbnail_create = request.POST.get('thumbnail_create')
+            thumbnail_delete = request.POST.get('thumbnail_delete')
+            # 썸네일 생성 요청 확인
+            if thumbnail_create:
+                # 생성요청 썸네일 리스트
+                thumbnail_create_list = thumbnail_create.split(',')
+                # S3 클라이언트 생성
+                s3client = S3Client()
+                # 생성요청 썸네일 리스트 Loop
+                for thumbnail_id in thumbnail_create_list:
+                    # 썸네일 정보 가져오기
+                    thumbnail_type = request.POST.get('thumbnail_type_' + thumbnail_id)
+                    thumbnail_url = request.POST.get('thumbnail_' + thumbnail_id)
+                    try:
+                        # 썸네일 저장
+                        file_name = make_filename(thumbnail_url)
+                        file_path = os.path.join(LOCAL_NETFLIX_THUMBNAIL, file_name)
+                        print(file_path)
+                        if not save_file_from_url(thumbnail_url, file_path):
+                            continue
+                        # 썸네일 S3 업로드
+                        if not s3client.upload_file(file_path, AWS_S3_NETFLIX_THUMBNAIL):
+                            continue
+                        # S3 업로드 성공 시 DB에 썸네일 정보 저장
+                        thumbnail_url = os.path.join(AWS_S3_NETFLIX_THUMBNAIL, file_name)
+                        thumbnail_extension = get_file_extension(file_name)
+                        thumbnail_size = get_file_size(file_path)
+                        thumbnail = video.thumbnail.create(
+                            type=thumbnail_type,
+                            url=thumbnail_url,
+                            extension=thumbnail_extension,
+                            size=thumbnail_size
+                        )
+                        thumbnail.save()
+                    except Exception as e:
+                        print(e)
+                # S3 클라이언트 종료
+                s3client.close()
+            # 썸네일 삭제 요청 확인
+            if thumbnail_delete:
+                # 생성요청 썸네일 리스트
+                thumbnail_delete_list = thumbnail_delete.split(',')
+                for thumbnail_id in thumbnail_delete_list:
+                    # 썸네일 삭제
+                    thumbnail = video.thumbnail.get(id=thumbnail_id)
+                    thumbnail.delete()
+            return HttpResponseRedirect(reverse('content:video-detail', kwargs={'pk': video_id}))
+        elif edit_type == "watch":
+            video_id = kwargs.get('pk')
+            video = Video.objects.get(id=video_id)
+            watch_create = request.POST.get('watch_create')
+            watch_delete = request.POST.get('watch_delete')
+            # 시청정보 생성 요청 확인
+            if watch_create:
+                # 생성요청 시청정보 리스트
+                watch_create_list = watch_create.split(',')
+                for watch_id in watch_create_list:
+                    try:
+                        # 시청정보 가져오기
+                        watch_type = request.POST.get('watch_type_' + watch_id)
+                        watch_url = request.POST.get('watch_' + watch_id)
+                        watch = video.watch.create(
+                            type=watch_type,
+                            url=watch_url
+                        )
+                        watch.save()
+                    except Exception as e:
+                        print(e)
+            # 시청정보 삭제 요청 확인
+            if watch_delete:
+                # 삭제요청 시청정보 리스트
+                watch_delete_list = watch_delete.split(',')
+                for watch_id in watch_delete_list:
+                    # 시청정보 삭제
+                    watch = video.watch.get(id=watch_id)
+                    watch.delete()
+            return HttpResponseRedirect(reverse('content:video-detail', kwargs={'pk': video_id}))
+        elif edit_type == "genre":
+            video_id = kwargs.get('pk')
+            video = Video.objects.get(id=video_id)
+            genre_create = request.POST.get('genre_create')
+            genre_delete = request.POST.get('genre_delete')
+            # 시청정보 생성 요청 확인
+            if genre_create:
+                # 생성요청 시청정보 리스트
+                genre_create_list = genre_create.split(',')
+                for genre_id in genre_create_list:
+                    try:
+                        # 시청정보 가져오기
+                        genre_type = request.POST.get('genre_type_' + genre_id)
+                        genre_name = request.POST.get('genre_' + genre_id)
+                        genre = video.genre.create(
+                            type=genre_type,
+                            url=genre_name
+                        )
+                        genre.save()
+                    except Exception as e:
+                        print(e)
+            # 시청정보 삭제 요청 확인
+            if genre_delete:
+                # 삭제요청 시청정보 리스트
+                genre_delete_list = genre_delete.split(',')
+                for genre_id in genre_delete_list:
+                    # 시청정보 삭제
+                    genre = video.genre.get(id=genre_id)
+                    genre.delete()
+            return HttpResponseRedirect(reverse('content:video-detail', kwargs={'pk': video_id}))
+        elif edit_type == "actor":
+            video_id = kwargs.get('pk')
+            video = Video.objects.get(id=video_id)
+            actor_create = request.POST.get('actor_create')
+            actor_delete = request.POST.get('actor_delete')
+            # 시청정보 생성 요청 확인
+            if actor_create:
+                # 생성요청 시청정보 리스트
+                actor_create_list = actor_create.split(',')
+                for actor_id in actor_create_list:
+                    try:
+                        # 시청정보 가져오기
+                        actor_type = request.POST.get('actor_type_' + actor_id)
+                        actor_name = request.POST.get('actor_' + actor_id)
+                        actor = video.actor.create(
+                            type=actor_type,
+                            url=actor_name
+                        )
+                        actor.save()
+                    except Exception as e:
+                        print(e)
+            # 시청정보 삭제 요청 확인
+            if actor_delete:
+                # 삭제요청 시청정보 리스트
+                actor_delete_list = actor_delete.split(',')
+                for actor_id in actor_delete_list:
+                    # 시청정보 삭제
+                    actor = video.actor.get(id=actor_id)
+                    actor.delete()
+            return HttpResponseRedirect(reverse('content:video-detail', kwargs={'pk': video_id}))
+        elif edit_type == "staff":
+            video_id = kwargs.get('pk')
+            video = Video.objects.get(id=video_id)
+            staff_create = request.POST.get('staff_create')
+            staff_delete = request.POST.get('staff_delete')
+            # 시청정보 생성 요청 확인
+            if staff_create:
+                # 생성요청 시청정보 리스트
+                staff_create_list = staff_create.split(',')
+                for staff_id in staff_create_list:
+                    try:
+                        # 시청정보 가져오기
+                        staff_type = request.POST.get('staff_type_' + staff_id)
+                        staff_url = request.POST.get('staff_' + staff_id)
+                        staff = video.staff.create(
+                            type=staff_type,
+                            url=staff_url
+                        )
+                        staff.save()
+                    except Exception as e:
+                        print(e)
+            # 시청정보 삭제 요청 확인
+            if staff_delete:
+                # 삭제요청 시청정보 리스트
+                staff_delete_list = staff_delete.split(',')
+                for staff_id in staff_delete_list:
+                    # 시청정보 삭제
+                    staff = video.staff.get(id=staff_id)
+                    staff.delete()
+            return HttpResponseRedirect(reverse('content:video-detail', kwargs={'pk': video_id}))
         else:
             return render(request, self.template_name)
 
--- static/js/content/video/edit.js
+++ static/js/content/video/edit.js
@@ -1,20 +1,200 @@
-const btnWatchDelete = document.querySelector('#btnWatchDelete');
-btnWatchDelete.addEventListener('click', () => {
-    const watchId = btnWatchDelete.dataset.watchId;
-    fetch(`/api/watch/${watchId}`, {
-        method: 'DELETE',
-    })
-        .then((res) => res.json())
-        .then((data) => {
-        if (data.success) {
-            location.href = '/content/video';
-        } else {
-            alert(data.message);
+// 사용 가능한 아이디 목록 (항목별 최대 10개)
+const newIds = {
+    "thumbnail": [
+        {"id": "new1", "usage": false},
+        {"id": "new2", "usage": false},
+        {"id": "new3", "usage": false},
+        {"id": "new4", "usage": false},
+        {"id": "new5", "usage": false},
+        {"id": "new6", "usage": false},
+        {"id": "new7", "usage": false},
+        {"id": "new8", "usage": false},
+        {"id": "new9", "usage": false},
+        {"id": "new10", "usage": false}
+    ],
+    "watch": [
+        {"id": "new1", "usage": false},
+        {"id": "new2", "usage": false},
+        {"id": "new3", "usage": false},
+        {"id": "new4", "usage": false},
+        {"id": "new5", "usage": false},
+        {"id": "new6", "usage": false},
+        {"id": "new7", "usage": false},
+        {"id": "new8", "usage": false},
+        {"id": "new9", "usage": false},
+        {"id": "new10", "usage": false}
+    ],
+    "genre": [
+        {"id": "new1", "usage": false},
+        {"id": "new2", "usage": false},
+        {"id": "new3", "usage": false},
+        {"id": "new4", "usage": false},
+        {"id": "new5", "usage": false},
+        {"id": "new6", "usage": false},
+        {"id": "new7", "usage": false},
+        {"id": "new8", "usage": false},
+        {"id": "new9", "usage": false},
+        {"id": "new10", "usage": false}
+    ],
+    "actor": [
+        {"id": "new1", "usage": false},
+        {"id": "new2", "usage": false},
+        {"id": "new3", "usage": false},
+        {"id": "new4", "usage": false},
+        {"id": "new5", "usage": false},
+        {"id": "new6", "usage": false},
+        {"id": "new7", "usage": false},
+        {"id": "new8", "usage": false},
+        {"id": "new9", "usage": false},
+        {"id": "new10", "usage": false}
+    ],
+    "staff": [
+        {"id": "new1", "usage": false},
+        {"id": "new2", "usage": false},
+        {"id": "new3", "usage": false},
+        {"id": "new4", "usage": false},
+        {"id": "new5", "usage": false},
+        {"id": "new6", "usage": false},
+        {"id": "new7", "usage": false},
+        {"id": "new8", "usage": false},
+        {"id": "new9", "usage": false},
+        {"id": "new10", "usage": false}
+    ],
+};
+
+// 썸네일 타입 정의
+const THUMBNAIL_TYPE = [
+    {"value": "10", "text": "세로"},
+    {"value": "11", "text": "가로"}
+];
+
+// 시청정보 타입 정의
+const WATCH_TYPE = [
+    {"value": "10", "text": "Netflix"},
+    {"value": "11", "text": "Disney+"},
+    {"value": "12", "text": "Tving"},
+    {"value": "13", "text": "Wavve"},
+];
+
+// 배우 타입 정의
+const ACTOR_TYPE = [
+    {"value": "10", "text": "주연"},
+    {"value": "11", "text": "조연"},
+    {"value": "12", "text": "단역"},
+];
+
+// 스태프 타입 정의
+const STAFF_TYPE = [
+    {"value": "10", "text": "감독"},
+    {"value": "11", "text": "작가"},
+    {"value": "12", "text": "제작"},
+];
+
+// 아이디 발행
+const publishId = (prefix) => {
+    let id = "";
+    for (const item of newIds[prefix]) {
+        if (!item.usage) {
+            id = item.id;
+            item.usage = true;
+            break;
         }
+    }
+    if (id) {
+        const createIds = document.querySelector(`input[name=${prefix}_create]`);
+        let createIdsArray = [];
+        if (createIds.value.length > 0) {
+            createIdsArray = createIds.value.split(',');
+        }
+        createIdsArray.push(id);
+        createIds.value = createIdsArray.join(',');
+    }
+
+    return id;
+}
+
+// 아이디 사용 중지
+const unpublishId = (prefix, id) => {
+    for (const item of newIds[prefix]) {
+        if (item.id === id) {
+            item.usage = false;
+        }
+    }
+    const createIds = document.querySelector(`input[name=${prefix}_create]`);
+    let createIdsArray = [];
+    if (createIds.value.length > 0) {
+        createIdsArray = createIds.value.split(',');
+    }
+    const index = createIdsArray.indexOf(id);
+    if (index > -1) {
+        createIdsArray.splice(index, 1);
+    }
+    createIds.value = createIdsArray.join(',');
+    return true;
+}
+
+// 엘리멘트 추가
+const addElement = (prefix, options = []) => {
+    const container = document.querySelector(`#${prefix}_container`);
+    const newDiv = document.createElement('div');
+
+    const id = publishId(prefix);
+    if (id === "") {
+        alert(`${prefix}는 최대 ${newIds[prefix].length}개까지 등록 가능합니다.`);
+        return;
+    }
+
+    newDiv.id = `${prefix}_wrap_${id}`;
+    newDiv.classList.add('flex', 'gap-2.5', 'w-full');
+
+    if (options.length > 0) {
+        const select = document.createElement('select');
+        select.name = `${prefix}_type_${id}`;
+        select.classList.add('form-select', 'w-[100px]');
+        options.map((option) => {
+            const opt = document.createElement('option');
+            opt.value = option.value;
+            opt.textContent = option.text;
+            select.appendChild(opt);
         });
+        newDiv.appendChild(select);
+    }
 
-});
-const btnWatchAdd = document.querySelector('#btnWatchAdd');
-btnWatchAdd.addEventListener('click', () => {
+    const newInput = document.createElement('input');
+    newInput.type = "text";
+    newInput.name = `${prefix}_${id}`;
+    newInput.classList.add('form-input', 'rounded-lg');
+    newDiv.appendChild(newInput);
 
-});
+    const newButton = document.createElement('button');
+    newButton.type = "button";
+    newButton.classList.add(
+        'bg-danger', 'text-white', 'rounded-[10px]', 'h-12', 'w-12', 'flex', 'justify-center', 'items-center'
+    );
+    newButton.textContent = "-";
+    newButton.dataset.id = id;
+    newButton.addEventListener('click', () => {
+        removeElement(prefix, id);
+    });
+    newDiv.appendChild(newButton);
+
+    container.appendChild(newDiv);
+}
+
+// 엘리멘트 삭제
+const removeElement = (prefix, id) => {
+    const container = document.querySelector(`#${prefix}_wrap_${id}`);
+    const origin = container.querySelector(`input[name=${prefix}_origin_${id}]`)
+    unpublishId(prefix, id);
+    if (origin !== null && origin !== undefined) {
+        const originId = id;
+        const deleteIds = document.querySelector(`input[name=${prefix}_delete]`);
+        let deleteIdsArray = [];
+        if (deleteIds.value.length > 0) {
+            deleteIdsArray = deleteIds.value.split(',');
+        }
+        deleteIdsArray.push(originId);
+        deleteIds.value = deleteIdsArray.join(',');
+    }
+    container.remove();
+}
