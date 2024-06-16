37521e645a39fb4480d0d4de8e65cf1e9ff22a12
UNANI <coder.unani@gmail.com>
2024-06-17 05:46:30 +0900

    [fix] 불필요한 temlplatetags 삭제

--- app/content/templatetags/common.py
+++
@@ -1,9 +0,0 @@
-from django import template
-from config.settings.settings import URL_THUMBNAIL
-
-register = template.Library()
-
-
-@register.filter
-def url_thumbnail(value):
-    return URL_THUMBNAIL + value
