53b8733fc2374743bd8bfa007d2965ef6af8e30f
UNANI <coder.unani@gmail.com>
2024-06-17 05:48:06 +0900

    [feat] / 경로 추가

--- config/urls.py
+++ config/urls.py
@@ -1,12 +1,14 @@
-from django.urls import path, include
-
 from django.contrib import admin
 from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import path, include
+
+from . import views
 
 urlpatterns = [
+    path("admin/", admin.site.urls),
     path("login/", LoginView.as_view(template_name="auth/login.html"), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
-    path("admin/", admin.site.urls),
+    path("", views.index, name="index"),
     path("api/", include('app.api.urls'), name='api'),
     path("builder/", include('app.builder.urls'), name='builder'),
     path("content/", include('app.content.urls'), name='content'),
