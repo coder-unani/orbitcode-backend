c800ffe18749c5e7d096235cdc49518632ee864d
UNANI <coder.unani@gmail.com>
2024-06-18 23:56:02 +0900

    [fix] Tving 관련 변수들 추가

--- config/properties.py
+++ config/properties.py
@@ -1,6 +1,10 @@
-URL_NETFLIX_LOGIN = "https://www.netflix.com/login"
-URL_NETFLIX_CONTENT = "https://www.netflix.com/kr/title"
-URL_NETFLIX_BOXOFFICE = "https://www.netflix.com/browse"
+NETFLIX_LOGIN_URL = "https://www.netflix.com/login"
+NETFLIX_CONTENT_URL = "https://www.netflix.com/kr/title"
+NETFLIX_BOXOFFICE_URL = "https://www.netflix.com/browse"
+
+TVING_LOGIN_URL = "https://user.tving.com/pc/user/otherLogin.tving?loginType=10&from=pc"
+TVING_CONTENT_URL = "https://www.tving.com/contents/"
+TVING_BOXOFFICE_URL = "https://www.tving.com/boxoffice"
 
 AWS_S3_VIDEO_THUMBNAIL = "video/thumbnail/"
 AWS_S3_NETFLIX_THUMBNAIL = AWS_S3_VIDEO_THUMBNAIL + "netflix/"
