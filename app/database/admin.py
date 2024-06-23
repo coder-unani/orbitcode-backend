e2c0c08bc5f44ce8bd73873503a7812e3a3502cc
UNANI <coder.unani@gmail.com>
2024-06-24 02:47:03 +0900

    [feat] admin 에 model 추가

--- app/database/admin.py
+++ app/database/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
 # Register your models here.
+from app.database.models import (
+    Actor,
+    Genre,
+    Staff,
+    Video,
+    VideoActor,
+    VideoGenre,
+    VideoStaff,
+    VideoThumbnail,
+    VideoWatch,
+    CountryCode
+)
+
+admin.site.register(Actor)
+admin.site.register(Genre)
+admin.site.register(Staff)
+admin.site.register(Video)
+admin.site.register(VideoActor)
+admin.site.register(VideoGenre)
+admin.site.register(VideoStaff)
+admin.site.register(VideoThumbnail)
+admin.site.register(VideoWatch)
+admin.site.register(CountryCode)
