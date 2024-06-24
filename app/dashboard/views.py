586a78a967597d57c3a91b89ed5ac4d1c5d857b6
UNANI <coder.unani@gmail.com>
2024-06-25 00:21:01 +0900

    [feat] 대시보드 GenericView 로 변경

--- app/dashboard/views.py
+++ app/dashboard/views.py
@@ -1,6 +1,33 @@
-from django.shortcuts import render
+import datetime
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import TemplateView
+
+from app.database.models import Video, Actor, Staff, User, VideoReview, VideoRating, VideoViewLog
+
+DJANGO_LOGIN_URL = "/login/"
+DJANGO_REDIRECT_FIELD_NAME = "next"
 
 
 # Create your views here.
-def index(request):
-    return render(request, "pages/dashboard/index.html")
+class Dashboard(LoginRequiredMixin, TemplateView):
+    login_url = DJANGO_LOGIN_URL
+    redirect_field_name = DJANGO_REDIRECT_FIELD_NAME
+    template_name = "pages/dashboard/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context['total_video'] = Video.objects.count()
+        context['total_video_confirm_false'] = Video.objects.filter(is_confirm=False).count()
+        context['total_actor'] = Actor.objects.count()
+        context['total_staff'] = Staff.objects.count()
+        context['total_user'] = User.objects.count()
+        context['total_review'] = VideoReview.objects.count()
+        context['total_rating'] = VideoRating.objects.count()
+        context['total_video_read'] = VideoViewLog.objects.count()
+        context['today_video_read'] = VideoViewLog.objects.filter(created_at__date=datetime.date.today()).count()
+        context['total_write_review'] = VideoReview.objects.count()
+        context['today_write_review'] = VideoReview.objects.filter(created_at__date=datetime.date.today()).count()
+
+        return context
