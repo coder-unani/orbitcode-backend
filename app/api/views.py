08577a753a48953c751d11358e0330bb8692b45b
UNANI <coder.unani@gmail.com>
2024-06-27 02:52:20 +0900

    [feat] s3 path builder 함수 생성

--- app/api/views.py
+++ app/api/views.py
@@ -1,11 +1,11 @@
+from PIL import Image
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from app.database.models import Actor, Staff
 from app.utils.uploader import S3ImageUploader
-from app.utils.utils import make_filename
-from config.constraints import AWS_S3_PATH_VIDEO_ACTOR, AWS_S3_PATH_VIDEO_STAFF
-from .serializers import ActorPictureUploadSerializer, StaffPictureUploadSerializer
+from app.utils.utils import make_s3_path
+from .serializers import VideoThumbnailUploadSerializer, ActorPictureUploadSerializer, StaffPictureUploadSerializer
 
 
 # Create your views here.
@@ -23,10 +23,7 @@ class ActorPictureUpload(APIView):
             try:
                 file = serializer.validated_data['file']
                 actor_id = serializer.validated_data['actor_id']
-                file_name = make_filename(file.name)
-                print(file.name)
-                print(file_name)
-                s3_path = AWS_S3_PATH_VIDEO_ACTOR + actor_id + "/" + file_name
+                s3_path = make_s3_path("actor", actor_id, file.name)
                 uploader = S3ImageUploader()
                 result = uploader.upload_from_file(file, s3_path, 300)
                 uploader.close()
@@ -49,8 +46,7 @@ class StaffPictureUpload(APIView):
             try:
                 file = serializer.validated_data['file']
                 staff_id = serializer.validated_data['staff_id']
-                file_name = make_filename(file.name)
-                s3_path = AWS_S3_PATH_VIDEO_STAFF + staff_id + "/" + file_name
+                s3_path = make_s3_path("staff", staff_id, file.name)
                 uploader = S3ImageUploader()
                 result = uploader.upload_from_file(file, s3_path, 300)
                 uploader.close()
@@ -63,3 +59,64 @@ class StaffPictureUpload(APIView):
                 return Response("Failed to upload the image", status=400)
         else:
             return Response("Failed to upload the image", status=400)
+
+
+class VideoThumbnailUpload(APIView):
+
+    def post(self, request, *args, **kwargs):
+        serializer = VideoThumbnailUploadSerializer(data=request.data)
+        if serializer.is_valid():
+            try:
+                file = serializer.validated_data['file']
+                video_id = serializer.validated_data['video_id']
+                s3_path = make_s3_path("video", video_id, file.name)
+                uploader = S3ImageUploader()
+                result = uploader.upload_from_file(file, s3_path, 300)
+                uploader.close()
+                if result is None:
+                    return Response("Failed to upload the image", status=400)
+                return Response(result, status=200)
+            except Exception as e:
+                print(e)
+                return Response("Failed to upload the image", status=400)
+
+
+# class VideoThumbnailUploadPre(APIView):
+#
+#     def post(self, request, *args, **kwargs):
+#         serializer = VideoThumbnailUploadSerializer(data=request.data)
+#         if serializer.is_valid():
+#             try:
+#                 file = serializer.validated_data['file']
+#                 video_id = serializer.validated_data['video_id']
+#                 s3_path = make_s3_path("video", video_id, file.name)
+#                 size = 300
+#                 with Image.open(file) as image:
+#                     image_format = image.format
+#                     image_width, image_height = image.size
+#                     aspect_ratio = image.height / image.width
+#                     if image.width >= image.height:  # 가로 이미지
+#                         width = size
+#                         height = int(size * aspect_ratio)
+#                     else:  # 세로 이미지
+#                         height = size
+#                         width = int(size / aspect_ratio)
+#                     image.resize((width, height), Image.Resampling.LANCZOS)
+#                     image_width, image_height = image.size
+#
+#                 return Response({
+#                     "url": s3_path,
+#                     "extension": image_format,
+#                     "width": image_width,
+#                     "height": image_height,
+#                     "size": len(file.read())
+#                 }, status=200)
+#
+#                 if result is None:
+#                     return Response("Failed to upload the image", status=400)
+#                 return Response(result, status=200)
+#             except Exception as e:
+#                 print(e)
+#                 return Response("Failed to upload the image", status=400)
+#         else:
+#             return Response("Failed to upload the image", status=400)
--- app/utils/utils.py
+++ app/utils/utils.py
@@ -4,22 +4,45 @@ from urllib.request import urlretrieve
 
 from django.utils.html import escape
 
+from config.constraints import AWS_S3_PATH_ACTOR, AWS_S3_PATH_STAFF, AWS_S3_PATH_VIDEO
+
 
 def html_escape(text):
     return escape(text)
 
 
-def make_filename(name):
+def make_s3_path(upload_type, key, file_name):
+    # 업로드 타입이 actor, staff, video가 아니면 False 반환
+    if upload_type not in ["actor", "staff", "video"]:
+        return False
+    # 파일명이나 키가 없으면 False 반환
+    if not file_name or not key:
+        return False
+    # 기본 경로
+    base_path = ""
+    # 업로드 타입에 따라 기본 경로 설정
+    if upload_type == "actor":
+        base_path = AWS_S3_PATH_ACTOR
+    if upload_type == "staff":
+        base_path = AWS_S3_PATH_STAFF
+    if upload_type == "video":
+        base_path = AWS_S3_PATH_VIDEO
+    # 기본 경로 끝이 /로 끝나지 않으면 /를 추가
+    if not base_path.endswith("/"):
+        base_path = f"{base_path}/"
+    # 기본 경로에 키 값 추가
+    base_path = f"{base_path}{key}/"
     # 파일명에서 확장자 추출
-    file_ext = name.split(".")[-1]
-    # 파라미터 삭제
+    file_ext = file_name.split(".")[-1]
+    # 확장자에서 파라메터 제거
     file_ext = file_ext.split("?")[0]
-    # 파라미터 삭제
     file_ext = file_ext.split("/")[0]
-    # 소문자로 변환
+    # 확장자 소문자 변환
     file_ext = file_ext.lower()
-    # 파일명 반환
-    return f"{uuid.uuid4()}.{file_ext}"
+    # 파일명
+    file_name = f"{uuid.uuid4()}.{file_ext}"
+    # S3 경로 반환
+    return f"{base_path}{file_name}"
 
 
 def save_file_from_url(file, file_path):
