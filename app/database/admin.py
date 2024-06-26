6cbdf8badc3ee20a0576f0b2bf69d714dfee0baa
UNANI <coder.unani@gmail.com>
2024-06-27 02:48:11 +0900

    [feat] admin 관리 모델 추가

--- app/database/admin.py
+++ app/database/admin.py
@@ -2,25 +2,29 @@ from django.contrib import admin
 
 # Register your models here.
 from app.database.models import (
-    Actor,
-    Genre,
-    Staff,
     Video,
+    Actor,
+    Staff,
+    Genre,
+    Production,
     VideoActor,
-    VideoGenre,
     VideoStaff,
+    VideoGenre,
+    VideoProduction,
     VideoThumbnail,
-    VideoWatch,
+    VideoPlatform,
     CountryCode
 )
 
-admin.site.register(Actor)
-admin.site.register(Genre)
-admin.site.register(Staff)
 admin.site.register(Video)
+admin.site.register(Actor)
+admin.site.register(Staff)
+admin.site.register(Genre)
+admin.site.register(Production)
 admin.site.register(VideoActor)
-admin.site.register(VideoGenre)
 admin.site.register(VideoStaff)
+admin.site.register(VideoGenre)
+admin.site.register(VideoProduction)
 admin.site.register(VideoThumbnail)
-admin.site.register(VideoWatch)
+admin.site.register(VideoPlatform)
 admin.site.register(CountryCode)
