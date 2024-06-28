9c88ef26b40b551371102db3bf26d540e60bdcc7
UNANI <coder.unani@gmail.com>
2024-06-28 16:05:00 +0900

    [feat] disney 메뉴 추가

--- app/builder/urls.py
+++ app/builder/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path("", view=views.Index.as_view(), name="index"),
     path("collect/netflix/", view=views.CollectNetflix.as_view(), name="collect-netflix"),
     path("collect/netflix/boxoffice", view=views.CollectNetflixBoxoffice.as_view(), name="collect-netflix-boxoffice"),
+    path("collect/disney/", view=views.CollectDisney.as_view(), name="collect-disney"),
     path("collect/tving/", view=views.CollectTving.as_view(), name="collect-tving"),
     path("collect/tving/boxoffice", view=views.CollectTvingBoxoffice.as_view(), name="collect-tving-boxoffice"),
 ]
